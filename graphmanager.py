deff5ee6d319c902700f399d32f46bd035eb8a17
JakubP <jakub.patzian.st@vsb.cz>
2023-04-29 22:06:52 +0200

    fixed remove_vertex

--- graphmanager.py
+++ graphmanager.py
@@ -1,12 +1,12 @@
-# space for imports
-import numpy as np
-
 class Graph:
     """
     Main class that holds a graph and all its vertices
     """
 
     class Vertex:
+        """
+        Vertex class, holds information about a single vertex
+        """
         def __init__(self):
             """
             Initialize vertex
@@ -34,29 +34,36 @@ class Graph:
         
         def set_edge(self, to_vertex: int, weight: float = 1):
             """
-            Change value of 
+            Change value of edge going to another vertex
             """
             self._neighbors[to_vertex] = weight
+
+        def remove_edge(self, to_vertex: int):
+            """
+            Remove an edge going to another vertex
+            """
+            if to_vertex in self._neighbors:
+                self._neighbors.pop(to_vertex)
 
     def __init__(self):
         """
         Initialize graph
         """
-        self._vertices: Graph.Vertex = []
+        self._vertices: list(Graph.Vertex) = []
 
     def __str__(self) -> str:
         ret = "Graph:\n"
         for i in range(len(self._vertices)):
-            ret += f"\t{i} - {self._vertices[i].__str__()}\n"
+            ret += f"{i:>4} - {self._vertices[i].__str__()}\n"
         return ret
 
     def __repr__(self) -> str:
         return self.__str__()
     
     def _wrong_index(self, index: int) -> bool:
-        if index < 0 or index >= len(self._vertices) or type(index != int):
+        if index < 0 or index >= len(self._vertices) or type(index) != int:
             return True
-        return True
+        return False
     
     def set_connection(self, from_vertex: int, to_vertex: int, value: float = 1, bidirectional: bool = False):
         """
@@ -73,11 +80,44 @@ class Graph:
         if bidirectional:
             self._vertices[to_vertex].set_edge(from_vertex, value)
 
+    def remove_connection(self, from_vertex: int, to_vertex: int, bidirectional: bool = False):
+        """
+        Remove an edge between two vertices
+        """
+        if self._wrong_index(from_vertex):
+            print("Invalid FROM index")
+            return
+        elif self._wrong_index(to_vertex):
+            print("Invalid TO vertex")
+            return
+        
+        self._vertices[from_vertex].remove_edge(to_vertex)
+        if bidirectional:
+            self._vertices[to_vertex].remove_edge(from_vertex)
+
+
     def add_vertex(self, loc_x: float = 0, loc_y: float = 0):
         """
         Add a single vertex
         """
-        self._vertices.append(Graph.Vertex())
+        v = Graph.Vertex()
+        v.move_vertex(loc_x, loc_y)
+        self._vertices.append(v)
+
+    def remove_vertex(self, index: int):
+        """
+        Remove a vertex
+        """
+        if self._wrong_index(index):
+            print("Invalid index")
+            return
+        
+        for v in self._vertices:
+            v.remove_edge(index)
+            v._neighbors = dict((neighbor if neighbor < index else neighbor-1, v._neighbors[neighbor]) for neighbor in v._neighbors)
+
+        self._vertices.pop(index)
+
 
     def move_vertex(self, index: int, new_x: float = 0, new_y: float = 0):
         """
